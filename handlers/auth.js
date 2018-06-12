b750510be04ce64f4496daf12707fc49be26fb13
John Paulino <johnpaulino@Johns-iMac.local>
2018-06-12 02:44:10 -0400

    Create signin route and handler

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,9 +1,39 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = function() {
-
-}
+exports.signin = async function(req, res, next) {
+  try {
+    let user = await db.User.findOne({
+      email: req.body.email
+    });
+    let { id, username } = user;
+    let isMatch = await user.comparePassword(req.body.password);
+    if (isMatch) {
+      let token = jwt.sign(
+        {
+          id,
+          username
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        id,
+        username,
+        token
+      });
+    } else {
+      return next({
+        status: 400,
+        message: "Invalid Email/Password"
+      });
+    }
+  } catch (err) {
+      return next({
+        status: 400,
+        message: "Invalid Email/Password"
+      });
+  }
+};
 
 exports.signup = async function(req, res, next) {
 
