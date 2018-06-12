9040a38de88ef9f0e163caa1053a6100cb3f9042
John Paulino <johnpaulino@Johns-iMac.local>
2018-06-12 00:23:49 -0400

    Create error handler in server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
 
 const PORT = process.env.PORT || 8081;
+
+app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname, "./rockus-client/build")));
 
 app.get("*", function(request, response) {
   response.sendFile(path.resolve(__dirname, "./rockus-client/build", "index.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
